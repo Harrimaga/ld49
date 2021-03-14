e254a86f403c81712aba0ee76e9057d3cb4664f8
Tim van Brederode <timvanbrederode@live.nl>
2021-03-14 20:16:18 +0100

    What is... SharpFont? (push so I can work on PC)

---
+++ Kee5Engine/FontService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SharpFont;
+
+namespace Kee5Engine
+{
+    public class FontService
+    {
+        Face face
+    }
+}
---
+++ Kee5Engine/TextRenderer2D.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Kee5Engine
+{
+    public class TextRenderer2D
+    {
+        private string _fontFolder;
+
+        public TextRenderer2D()
+        {
+            _fontFolder = "Fonts/";
+        }
+    }
+}
