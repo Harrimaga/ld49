4084fbe8f5939ef10cb1d7f182e1fb0140b4c4f8
KingTurtleTheFrog <timvanbrederode@live.nl>
2021-10-03 14:18:44 +0200

    Menu part 1

--- Kee5Engine/Logic/Menus/MainMenu.cs
+++ Kee5Engine/Logic/Menus/MainMenu.cs
@@ -11,6 +11,10 @@ namespace Kee5Engine
         public Button start, selectLevel, fullScreen, quit;
         public bool isFullScreen;
         public Texture levelTime;
+        public List<Button> levels, activeLevelButtons;
+        public int currentLevelList;
+
+        private int levelsPerList = 5;
 
         public MainMenu()
         {
@@ -20,6 +24,10 @@ namespace Kee5Engine
             string fs = (isFullScreen ? "ON" : "OFF");
             fullScreen = new Button(Window.WindowSize.X / 2, Window.WindowSize.Y / 2 + 120, 240, 50, 1, "Pixel", $"Fullscreen {fs}", Vector4.One, new Vector3(0, 0, 0), TextAlignment.CENTER, true, () => { ToggleFullscreen(); });
             quit = new Button(Window.WindowSize.X / 2, Window.WindowSize.Y - 100, 240, 50, 1, "Pixel", "Quit Game", Vector4.One, new Vector3(0, 0, 0), TextAlignment.CENTER, true, () => { Game.gameWindow.Close(); });
+
+            levels = new List<Button>();
+            activeLevelButtons = new List<Button>();
+            currentLevelList = 0;
             Globals.activeButtons.Add(start);
             Globals.activeButtons.Add(selectLevel);
             Globals.activeButtons.Add(fullScreen);
@@ -50,6 +58,38 @@ namespace Kee5Engine
             isFullScreen = !isFullScreen;
         }
 
+        public void LevelList(int i)
+        {
+            if (i < 0)
+            {
+                i++;
+                return;
+            }
+
+            if (i > (int)Math.Floor((double)(Globals.levelsUnlocked - 1) / (float)levelsPerList))
+            {
+                i--;
+                return;
+            }
+
+            foreach (Button button in activeLevelButtons)
+            {
+                Globals.activeButtons.Remove(button);
+            }
+
+            for (int j = 0; j < levelsPerList; j++)
+            {
+                if (i * levelsPerList + j > Globals.levelsUnlocked - 1)
+                {
+                    return;
+                }
+                int level = i * levelsPerList + j;
+                Button btn = new Button(Window.WindowSize.X / 2, Window.WindowSize.Y / 2 + 60 * j, 200, 50, 1, "Pixel", Balance.levels[level], Vector4.One, new Vector3(0, 0, 0), TextAlignment.CENTER, true, () => { StartGame(level); });
+                Globals.activeButtons.Add(btn);
+                activeLevelButtons.Add(btn);
+            }
+        }
+
 
         public void OpenLevelSelect(double time = 0)
         {
@@ -64,13 +104,23 @@ namespace Kee5Engine
                 levelTime = Texture.LoadFromBmp(Window.textRenderer.RenderString(timestring, Color.White), "levelTime", false);
             }
 
-            for (int i = 0; i < Globals.levelsUnlocked; i++)
-            {
-                int level = i;
-                Globals.activeButtons.Add(
-                    new Button(Window.WindowSize.X / 2, Window.WindowSize.Y / 2 + 60 * i, 200, 50, 1, "Pixel", Balance.levels[level], Vector4.One, new Vector3(0, 0, 0), TextAlignment.CENTER, true, () => { StartGame(level); })
-                    );
-            }
+            //for (int i = 0; i < Globals.levelsUnlocked; i++)
+            //{
+            //    int level = i;
+            //    levels.Add(
+            //        new Button(Window.WindowSize.X / 2, Window.WindowSize.Y / 2, 200, 50, 1, "Pixel", Balance.levels[level], Vector4.One, new Vector3(0, 0, 0), TextAlignment.CENTER, true, () => { StartGame(level); })
+            //        );
+            //}
+
+            LevelList(0);
+
+            Globals.activeButtons.Add(
+                new Button(Window.WindowSize.X / 2 - 50, Window.WindowSize.Y - 200, 80, 50, 1, "Pixel", "<", Vector4.One, Vector3.Zero, TextAlignment.CENTER, true, () => { LevelList(--currentLevelList); })
+                );
+
+            Globals.activeButtons.Add(
+                new Button(Window.WindowSize.X / 2 + 50, Window.WindowSize.Y - 200, 80, 50, 1, "Pixel", ">", Vector4.One, Vector3.Zero, TextAlignment.CENTER, true, () => { LevelList(++currentLevelList); })
+                );
 
             Globals.activeButtons.Add(
                 new Button(Window.WindowSize.X / 2, Window.WindowSize.Y - 100, 200, 50, 1, "Pixel", "Main Menu", Vector4.One, new Vector3(0, 0, 0), TextAlignment.CENTER, true, () => {
